7c3a88c5678f1569af40903494fa1218aafcbfba
Shubham Agrawal <shubhama@geekyants.com>
2020-09-26 23:17:37 +0530

    Added Code Mirror and local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './home/Home';
+import './App.scss';
 
 const App = () => <Home />;
 
--- src/pure-components/Editor/Editor.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const Editor = () => (
-  <h1>Editor!!</h1>
-);
-export default Editor;
